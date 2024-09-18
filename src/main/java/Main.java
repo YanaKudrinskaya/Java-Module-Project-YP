796f09c2af1144f654537bb67f553c92edfb8e58
Yana <yanakudrinskaya86@yandex.ru>
2024-09-18 13:21:41 +0300

    Проектная работа 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,44 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        Race race = new Race();
+
+        for(int i = 1; i <= 3; i++) {
+            System.out.println("Введите название " + i + " автомобиля");
+            String name  = scanner.nextLine();
+
+            while(true) {
+                System.out.println("Введите скорость " + i + " автомобиля");
+                int speed = scanner.nextInt();
+                if (speed > 0 && speed <=250) {
+                    Automobile automobile = new Automobile(name, speed);
+                    race.leader(automobile);
+                    break;
+                } else System.out.println("Скорость автомобиля должна быть больше 0 и не больше 250");
+            }
+        }
+        System.out.println("Победитель гонки  " + race.winnerName);
+    }
+}
+class Automobile {
+    String name;
+    int speed;
+
+    Automobile (String name, int speed) {
+        this.name = name;
+        this.speed = speed;
+    }
+}
+class Race {
+    String winnerName = "";
+    int distance = 0;
+    public void leader (Automobile automobile) {
+        int newDistance = 24 * automobile.speed;
+        if (newDistance > distance) {
+            distance = newDistance;
+            winnerName = automobile.name;
+        }
     }
 }
