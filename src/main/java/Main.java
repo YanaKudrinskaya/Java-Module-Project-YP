58ddef6d52290b3ab0433af63c8b274966780aa0
Yana <yanakudrinskaya86@yandex.ru>
2024-09-18 23:00:21 +0300

    Исправление замечаний по ревью

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,36 +5,49 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Race race = new Race();
 
-        for(int i = 1; i <= 3; i++) {
+        for (int i = 1; i <= 3; i++) {
             System.out.println("Введите название " + i + " автомобиля");
-            String name  = scanner.nextLine();
+            scanner.next();
+            String name = scanner.nextLine();
 
-            while(true) {
+            while (true) {
                 System.out.println("Введите скорость " + i + " автомобиля");
-                int speed = scanner.nextInt();
-                if (speed > 0 && speed <=250) {
-                    Automobile automobile = new Automobile(name, speed);
-                    race.leader(automobile);
-                    break;
-                } else System.out.println("Скорость автомобиля должна быть больше 0 и не больше 250");
+                int speed;
+                if (!scanner.hasNextInt()) {
+                    System.out.println("Неверная скорость");
+                    scanner.next();
+                    continue;
+                }
+                speed = scanner.nextInt();
+                if (speed <= 0 || speed > 250) {
+                    System.out.println("Неверная скорость. Введите целое число от 1 до 250");
+                    continue;
+                }
+                Automobile automobile = new Automobile(name, speed);
+                race.leader(automobile);
+                break;
             }
         }
         System.out.println("Победитель гонки  " + race.winnerName);
+        scanner.close();
     }
 }
+
 class Automobile {
     String name;
     int speed;
 
-    Automobile (String name, int speed) {
+    Automobile(String name, int speed) {
         this.name = name;
         this.speed = speed;
     }
 }
+
 class Race {
     String winnerName = "";
     int distance = 0;
-    public void leader (Automobile automobile) {
+
+    public void leader(Automobile automobile) {
         int newDistance = 24 * automobile.speed;
         if (newDistance > distance) {
             distance = newDistance;
